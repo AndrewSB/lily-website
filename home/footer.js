375ce8e38af7a25fd3b6d07cab56c5eaca4903ce
Andrew Breckenridge <asbreckenridge@me.com>
2016-04-08 16:54:27 -0700

    add footer

---
+++ home/footer.js
@@ -0,0 +1,12 @@
+view Home.Footer {
+
+  <h4>Lily</h4>
+  
+  <p>Made with ♥️ in sf</p>
+
+  $ = {
+    height: 100,
+    width: '100%',
+    background: '#50ABF1',
+  }
+}
--- main.js
+++ main.js
@@ -2,4 +2,5 @@ view Main {
   <Home.Head />
   <Home.Demo />
   <Home.Expose />
+  <Home.Footer />
 }
