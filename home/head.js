7ab90366e311616e44a0834280ac06f0aaaf1845
patrickdaviddaniel <patrickvdaniel@gmail.com>
2016-04-11 14:54:50 -0700

    update main backgroundImage

--- home/head.js
+++ home/head.js
@@ -2,7 +2,7 @@ import { Banner } from 'rebass'
 view Home.Head {
   <Banner
     align="center"
-    backgroundImage="http://i.imgur.com/grcAoTT.jpg?1">
+    backgroundImage="http://i.imgur.com/W98uxfx.jpg?1">
     <h1>Lily</h1>
     <h2>We make incorporation short and easy</h2>
   </Banner>
