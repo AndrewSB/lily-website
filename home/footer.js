fbe582a38c887ce10500dc73d6431328de2da466
Andrew Breckenridge <asbreckenridge@me.com>
2016-04-15 15:03:44 -0700

    fix footer mobile responsiveness

--- home/footer.js
+++ home/footer.js
@@ -2,43 +2,38 @@ import { styles } from '../constants'
 import { copy } from '../content'
 
 view Home.Footer {
-  <left>
+  <section>
     <h3>{copy.footer.left.title}</h3>
     <p>{copy.footer.left.description}</p>
-  </left>
-  <center>
+  </section>
+  <section>
     <h3>{copy.footer.center.title}</h3>
     <a href={'mailto:' + copy.footer.center.email}>{copy.footer.center.email}</a>
     <a href={'sms:' + copy.footer.center.phoneNumber}>{copy.footer.center.phoneNumber}</a>
-  </center>
-  <right>
+  </section>
+  <section>
     <h3>{copy.footer.right.title}</h3>
     <a href={'mailto:' + copy.footer.right.email}>{copy.footer.right.email}</a>
-  </right>
+  </section>
 
   $ = [styles.flexAndCenter, {
       flexDirection: 'row',
+      flexWrap: 'wrap',
       justifyContent: 'center',
       padding: [50, 22],
       color: 'white',
       background: '#262626',
   }]
 
-  const commonStyle = [styles.flexAndCenter, {
+  $section = [styles.flexAndCenter, {
     flexDirection: 'column',
     textAlign: 'center',
-    width: '30%',
-    // padding: [0, "3%"],
+    width: 300,
   }]
-
-  $left = commonStyle
-  $center = commonStyle
-  $right = commonStyle
 
   const colorAndPadding = {
     color: '#818181',
     padding: [8, 0],
-
   }
 
   $a = colorAndPadding
